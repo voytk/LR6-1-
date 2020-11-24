976f284d04fa2012c64cafaeb2ddd14a2efabe04
Alice Voytkevich <71379424+voytk@users.noreply.github.com>
2020-11-24 17:25:44 +0200

    1

---
+++ 1part.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+     public class Program
+    {
+        public class AbstractHandler
+        {
+            void Open()
+            {
+
+            }
+            void Create()
+            {
+
+            }
+            void Change()
+            {
+
+            }
+            void Save()
+            {
+
+            }
+        }  
+       public static void Main()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
